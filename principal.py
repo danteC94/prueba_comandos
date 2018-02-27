26b7405dd84d45b0c0d05056e1da93b85d13bf58
danteC94 <dacanizo94@gmail.com>
2018-02-26 21:26:54 -0300

    segundo_commit_temporal

---
+++ principal.py
@@ -0,0 +1,2 @@
+def show(cadena):
+    return cadena
---
+++ test_principal.py
@@ -0,0 +1,12 @@
+import unittest
+from principal import show
+
+
+class TestPrincipal(unittest.TestCase):
+    def test_print(self):
+        resultado= show("hola mundo")
+        self.assertEqual(resultado, "hola mundo")
+
+
+if __name__ == "__main__":
+    unittest.main()
