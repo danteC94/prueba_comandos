48c3b30be5f26a071ccfa2b1492e921000951765
danteC94 <dacanizo94@gmail.com>
2018-02-26 21:33:51 -0300

    segundo_test_case

--- test_principal.py
+++ test_principal.py
@@ -4,8 +4,12 @@ from principal import show
 
 class TestPrincipal(unittest.TestCase):
     def test_print(self):
-        resultado= show("hola mundo")
+        resultado = show("hola mundo")
         self.assertEqual(resultado, "hola mundo")
+
+    def test_print(self):
+        resultado = show("Bienvenido")
+        self.assertEqual(resultado, "Bienvenido")
 
 
 if __name__ == "__main__":
