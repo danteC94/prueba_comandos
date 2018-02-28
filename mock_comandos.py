7828ddb232b8a8b9c635ecb10c8610ff7689d128
danteC94 <dacanizo94@gmail.com>
2018-02-28 18:34:55 -0300

    agregando la suite

---
+++ mock_comandos.py
@@ -0,0 +1,9 @@
+hola = 'cadena'
+
+if(raw_input('hola ingresa un numero') == '7'):
+    print "\nExcelente\n"
+else:
+    print "\nNo es el numero esperado\n"
+
+if(isinstance(hola, str)):
+    print "acertaste otra vez"
